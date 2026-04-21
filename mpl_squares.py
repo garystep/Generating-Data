b4b142214acac4d23efab2def8ae3137afdc8807
garystep <sg6787944@gmail.com>
2026-04-21 14:41:01 -0400

    styled tick marks

--- mpl_squares.py
+++ mpl_squares.py
@@ -10,4 +10,7 @@ ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
+# set size of tick labels.
+ax.tick_params(axis="both", labelsize=14)
+
 plt.show()
