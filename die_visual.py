16b997995f3f607c61ba4cd27a2336ca644cd8cf
garystep <sg6787944@gmail.com>
2026-04-24 12:58:52 -0400

    rolling two d6 dies

--- die_visual.py
+++ die_visual.py
@@ -1,25 +1,27 @@
 import plotly.express as px
 
 from die import Die
+import die
 
 # Create a D6
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-for value in range(1, die.num_sides + 1):
+for value in range(2, 2 * die_1.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 
 # Visualize the results.
-title = "Results of rolling a D6 1000 times"
+title = "Results of rolling two D6 1000 times"
 labels = {"x": "Result", "y": "Frequency"}
-fig = px.bar(x=range(1, die.num_sides + 1), y=frequencies, title=title, labels=labels)
+fig = px.bar(x=range(2, 2 * die_1.num_sides + 1), y=frequencies, title=title, labels=labels)
 fig.show()
