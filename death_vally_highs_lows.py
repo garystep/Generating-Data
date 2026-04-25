2cc4e49088be0b7eae7ac1f1bb890f1f2091b419
garystep <sg6787944@gmail.com>
2026-04-25 12:54:35 -0400

    death valley visual

---
+++ death_vally_highs_lows.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+path = Path('weather_data/death_valley_2021_simple.csv')
+lines = path.read_text(encoding='utf-8').splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+            print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Format plot.
+ax.set_title("Daily high and low temperatures, 2021\nDeath Valley, CA", fontsize=16)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
