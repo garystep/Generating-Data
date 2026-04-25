820e3c52995791db3427ae71f442ba0604ea7a37
garystep <sg6787944@gmail.com>
2026-04-25 12:12:49 -0400

    plotting temperatures using matplotlib

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt
+
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
@@ -12,4 +14,15 @@ for row in reader:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot.
+ax.set_title("Daily high temperatures, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
