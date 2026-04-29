fe73c6d988d493b7b20571083623c9b3bc50f84f
garystep <sg6787944@gmail.com>
2026-04-29 12:50:41 -0400

    simple api call to github.com

---
+++ python_repos.py
@@ -0,0 +1,15 @@
+import requests
+
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories'
+url += '?q=language:python&sort=stars:>10000'
+
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)  
+print(f'Status code: {r.status_code}')
+
+# Convert the response to a dictionary and print the number of repositories.
+response_dict = r.json()
+
+# Process results.
+print(f'Total repositories: {response_dict["total_count"]}')
