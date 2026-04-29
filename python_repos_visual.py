92d4d2073543aded731ca8c5b3b3106ebc6432c0
garystep <sg6787944@gmail.com>
2026-04-29 13:10:49 -0400

    using ploty to graph github repos

---
+++ python_repos_visual.py
@@ -0,0 +1,25 @@
+import requests
+import plotly.express as px
+
+# Make an API call and store the response.
+url = 'https://api.github.com/search/repositories'
+url += '?q=language:python&sort=stars:>10000'
+
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)  
+print(f'Status code: {r.status_code}')
+
+# Process results.
+response_dict = r.json()    
+print(f'Total repositories: {response_dict["total_count"]}')
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Create a bar chart to visualize the star counts.
+fig = px.bar(x=repo_names, y=stars, labels={'x': 'Repository', 'y': 'Stars'})
+fig.show()
