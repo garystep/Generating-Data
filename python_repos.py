ad9d7c5d5c811ad497ba85584c8481fbcd09eabd
garystep <sg6787944@gmail.com>
2026-04-29 13:04:06 -0400

    pulling out specific info from github

--- python_repos.py
+++ python_repos.py
@@ -13,3 +13,15 @@ response_dict = r.json()
 
 # Process results.
 print(f'Total repositories: {response_dict["total_count"]}')
+print(f'Items in this response: {len(response_dict["items"])}')
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f'\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+    print(f'\nName: {repo_dict["name"]}')
+    print(f'Owner: {repo_dict["owner"]["login"]}')
+    print(f'Stars: {repo_dict["stargazers_count"]}')
+    print(f'Repository: {repo_dict["html_url"]}')
+    print(f'Description: {repo_dict["description"]}')
+
